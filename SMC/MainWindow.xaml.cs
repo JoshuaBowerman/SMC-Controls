7039d7a5120f3795a7091a877028fda9d50247fe
JoshuaBowerman <JoshuaBowerman@users.noreply.github.com>
2020-04-13 15:13:09 -0400

    Created Program
    Made Simple Tool

--- SMC/MainWindow.xaml.cs
+++ SMC/MainWindow.xaml.cs
@@ -25,7 +25,23 @@ namespace SMC
         {
             InitializeComponent();
         }
-
+        private void command(string ip,string user,string command)
+        {
+            //BASS 192.168.0.8
+            SshClient sshclient = new SshClient(ip, user, passwordBox.Password);
+            try
+            {
+                sshclient.Connect();
+                SshCommand sc = sshclient.CreateCommand(command);
+                sc.Execute();
+                LogBlock.Text = LogBlock.Text + sc.Result;
+                sshclient.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                LogBlock.Text = LogBlock.Text + ex.Message + "\n";
+            }
+        }
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             //BASS 192.168.0.8
@@ -33,7 +49,7 @@ namespace SMC
             try
             {
                 sshclient.Connect();
-                SshCommand sc = sshclient.CreateCommand("sudo reboot");
+                SshCommand sc = sshclient.CreateCommand("reboot");
                 sc.Execute();
                 LogBlock.Text = LogBlock.Text + "\nSent Reboot Command";
                 sshclient.Disconnect();
@@ -52,7 +68,7 @@ namespace SMC
             try
             {
                 sshclient.Connect();
-                SshCommand sc = sshclient.CreateCommand("sudo reboot");
+                SshCommand sc = sshclient.CreateCommand("reboot");
                 sc.Execute();
                 LogBlock.Text = LogBlock.Text + "\nSent Reboot Command";
                 sshclient.Disconnect();
@@ -71,7 +87,7 @@ namespace SMC
             try
             {
                 sshclient.Connect();
-                SshCommand sc = sshclient.CreateCommand("sudo reboot");
+                SshCommand sc = sshclient.CreateCommand("reboot");
                 sc.Execute();
                 LogBlock.Text = LogBlock.Text + "\nSent Reboot Command";
                 sshclient.Disconnect();
@@ -90,7 +106,7 @@ namespace SMC
             try
             {
                 sshclient.Connect();
-                SshCommand sc = sshclient.CreateCommand("sudo reboot");
+                SshCommand sc = sshclient.CreateCommand("reboot");
                 sc.Execute();
                 LogBlock.Text = LogBlock.Text + "\nSent Reboot Command";
                 sshclient.Disconnect();
@@ -104,40 +120,70 @@ namespace SMC
 
         private void VPN_Click(object sender, RoutedEventArgs e)
         {
-            //BASS 192.168.0.8
-            SshClient sshclient = new SshClient("192.168.0.8", "dietpi", passwordBox.Password);
-            try
-            {
-                sshclient.Connect();
-                SshCommand sc = sshclient.CreateCommand("expressvpn connect");
-                sc.Execute();
-                LogBlock.Text = LogBlock.Text + "\n" + sc.Result;
-                sshclient.Disconnect();
-            }
-            catch (Exception ex)
-            {
-                LogBlock.Text = LogBlock.Text + "\n" + ex.Message;
-            }
-
+            command("192.168.0.8", "dietpi", "expressvpn connect");
         }
 
         private void Update1_Click(object sender, RoutedEventArgs e)
         {
             //BASS 192.168.0.8
-            SshClient sshclient = new SshClient("192.168.0.8", "dietpi", passwordBox.Password);
-            try
-            {
-                sshclient.Connect();
-                SshCommand sc = sshclient.CreateCommand("sudo apt-get update && sudo apt-get upgrade&");
-                sc.Execute();
-                LogBlock.Text = LogBlock.Text + "\n" + sc.Result;
-                sshclient.Disconnect();
-            }
-            catch (Exception ex)
-            {
-                LogBlock.Text = LogBlock.Text + "\n" + ex.Message;
-            }
+            command("192.168.0.8", "dietpi", "sudo apt-get update && sudo apt-get upgrade&");
+        }
 
+        private void Update2_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.9", "root", "kodi-send --action=UpdateAddonRepos && kodi-send --action=UpdateLocalAddons");
+        }
+
+        private void UpdateLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.9", "root", "kodi-send --action=\"UpdateLibrary(video)\"");
+            command("192.168.0.7", "root", "kodi-send --action=\"UpdateLibrary(video)\"");
+            command("192.168.0.6", "root", "kodi-send --action=\"UpdateLibrary(video)\"");
+        }
+
+        private void On1_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.9", "root", "kodi-send --action=CECActivateSource");
+        }
+
+        private void On2_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.7", "root", "kodi-send --action=CECActivateSource");
+        }
+
+        private void On3_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.6", "root", "kodi-send --action=CECActivateSource");
+        }
+
+        private void Off1_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.9", "root", "kodi-send --action=CECStandby");
+        }
+
+        private void Off2_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.7", "root", "kodi-send --action=CECStandby");
+        }
+
+        private void Off3_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.6", "root", "kodi-send --action=CECStandby");
+        }
+
+        private void Send1_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.9", "root", "kodi-send --action=\"Notification(Notification," + Message.Text + ")\"");
+        }
+
+        private void Send2_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.7", "root", "kodi-send --action=\"Notification(Notification," + Message.Text + ")\"");
+        }
+
+        private void Send3_Click(object sender, RoutedEventArgs e)
+        {
+            command("192.168.0.6", "root", "kodi-send --action=\"Notification(Notification," + Message.Text + ")\"");
         }
     }
 }
